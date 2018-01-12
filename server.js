50ab5c44c263165f625cd719b8d2ef48d838a050
wjdgkals23 <wjdgkals74@gmail.com>
2018-01-12 23:53:18 +0900

    practice node.js

---
+++ server.js
@@ -0,0 +1,14 @@
+const http = require('http');
+// require http 라는 모듈을 http라는 상수(할당되면 변하지 않음)에 담은 것
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
---
+++ uglified.js
@@ -0,0 +1,3 @@
+function hello(l){console.log("hi"+l)}hello("하민");
+//지역변수 줄이기 -m
+//저장하기 uglifyjs 파일이름 -o 저장할 이름
